71b35b95bcba10b2145443108c794e665f9d5318
Andrew Livesay <andrew.livesay@gmail.com>
2016-02-29 06:15:20 +0000

    added ldap auth support

--- server/config/validator.js
+++ server/config/validator.js
@@ -1,13 +1,72 @@
 var Mentat = require('mentat');
+var ldap = require('ldapjs');
+
+var ldapServer = ldap.createServer();
+
+function ldapFindUser(username, callback) {
+    var client = ldap.createClient({
+        url: 'ldaps://' + Mentat.settings.auth.ldap.server + ':' + Mentat.settings.auth.ldap.port,
+        tlsOptions: {
+            rejectUnauthorized: !!Mentat.settings.auth.ldap.rejectUnauthorized
+        }
+    });
+
+    var opts = {
+        filter: (Mentat.settings.auth.ldap.ldapFilterAttribute || 'sAMAccountName') + '=' + username,
+        scope: 'sub'
+    };
+
+    var rootDN = Mentat.settings.auth.ldap.rootDN || '';
+    var targetDN;
+    var serviceAccountDN = Mentat.settings.auth.ldap.serviceAccountDN;
+    var serviceAccountPassword = Mentat.settings.auth.ldap.serviceAccountPassword;
+
+    client.bind(serviceAccountDN, serviceAccountPassword, function initialBindDone (err) {
+        if (err) {
+            return callback(false, err);
+        }
+
+        client.search(rootDN, opts, function ldapSearchDone (err, result) {
+            if (err) {
+                return callback(false, err);
+            }
+
+            result.on('error', function onSearchError (err) {
+                return callback(false, err);
+            });
+
+            result.on('searchEntry', function onSearchEntry (entry) {
+                targetDN = entry.objectName;
+            });
+
+            result.on('end', function onSearchEnd (entry) {
+                client.unbind();
+
+                if (!targetDN) {
+                    return callback(false, 'invalid user');
+                }
+
+                return callback(true);
+            });
+        });
+    });
+}
+
 
 module.exports = function (decoded, request, callback) {
   Mentat.models.User
     .findOne({where: { username: decoded.username }})
     .nodeify(function (err, user) {
-      if (err || !user) {
-        return callback(null, false);
+      if (user && !err) {
+        return callback(null, true);
       }
 
-      return callback(null, true);
+      if (Mentat.settings.auth.useLDAP) {
+        ldapFindUser(decoded.username, function findUserDone (result, err) {
+          return callback(null, true);
+        });
+      } else {
+        return callback(null, false);
+      }
     });
 };
--- server/handlers/api/auth.handler.js
+++ server/handlers/api/auth.handler.js
@@ -3,8 +3,94 @@ var Boom = require('boom');
 var bcrypt = require('bcrypt');
 var jwt = require('jsonwebtoken');
 var redis = require('redis');
+var ldap = require('ldapjs');
 
 var client = redis.createClient();
+
+var ldapServer = ldap.createServer();
+
+// TODO: this all needs to be refactored O_o
+
+function ldapAuth(username, password, callback) {
+    var client = ldap.createClient({
+        url: 'ldaps://' + Mentat.settings.auth.ldap.server + ':' + Mentat.settings.auth.ldap.port,
+        tlsOptions: {
+            rejectUnauthorized: !!Mentat.settings.auth.ldap.rejectUnauthorized
+        }
+    });
+
+    var ldapFilterAttribute = Mentat.settings.auth.ldap.ldapFilterAttribute || 'sAMAccountName';
+    var opts = {
+        filter: '(&(objectclass=user)('+ ldapFilterAttribute + '=' + username + '))',
+        scope: 'sub'
+    };
+
+    var rootDN = Mentat.settings.auth.ldap.rootDN || '';
+    
+    var targetDN;
+    var serviceAccountDN = Mentat.settings.auth.ldap.serviceAccountDN;
+    var serviceAccountPassword = Mentat.settings.auth.ldap.serviceAccountPassword;
+
+    client.bind(serviceAccountDN, serviceAccountPassword, function initialBindDone (err) {
+        if (err) {
+            return callback(false, err);
+        }
+
+        client.search(rootDN, opts, function ldapSearchDone (err, result) {
+            if (err) {
+                return callback(false, err);
+            }
+
+            result.on('error', function onSearchError (err) {
+                return callback(false, err);
+            });
+
+            result.on('searchEntry', function onSearchEntry (entry) {
+                targetDN = entry.objectName;
+            });
+
+            result.on('end', function onSearchEnd (entry) {
+
+                if (!targetDN) {
+                    return callback(false, 'invalid user');
+                }
+
+                client.bind(targetDN, password, function ldapBindDone (err) {
+                    client.unbind();
+
+                    if (err) {
+                        return callback(false, 'invalid password');
+                    }
+
+                    return callback(true);
+                });
+            });
+        });
+    });
+}
+
+function ldapAuthResponder(username, password, callback) {
+    ldapAuth(username, password, function ldapAuthDone (result, err) {
+        if (result) {
+            return callback(createJwtToken(username));
+        }
+
+        console.log('XXX: ' + err);
+        return callback(Boom.unauthorized(err));
+    });
+}
+
+function createJwtToken(username) {
+    var tokenPayload = {
+        username: username
+    };
+
+    var token = jwt.sign(tokenPayload, Mentat.settings.auth.key, {
+        expiresIn: Mentat.settings.auth.tokenTTL
+    });
+    
+    return { token: token };
+}
 
 module.exports = new Mentat.Handler('Auth', {
     routes: [
@@ -21,20 +107,16 @@ module.exports = new Mentat.Handler('Auth', {
                 }
 
                 if (!user) {
-                    return reply(Boom.unauthorized('invalid user'));
+                    if (!!Mentat.settings.auth.useLDAP) {
+                       return ldapAuthResponder(request.payload.username, request.payload.password, reply); 
+                    } else {
+                        return reply(Boom.unauthorized('invalid user'));
+                    }
                 }
 
                 if (bcrypt.compareSync(request.payload.password, user.password)) {
-                    var tokenPayload = {
-                        username: user.username
-                    };
-
-                    var token = jwt.sign(tokenPayload, Mentat.settings.auth.key, {
-                        expiresIn: Mentat.settings.auth.tokenTTL
-                    });
-
-                    return reply({token: token});
-                }
+                    return reply(createJwtToken(user.username));
+                } 
 
                 return reply(Boom.unauthorized('invalid password'));
             });
