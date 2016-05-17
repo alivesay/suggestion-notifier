cc646fd7f246a9e0f995a4b7d0bad35706e146bf
Andrew Livesay <andrew.livesay@gmail.com>
2016-05-17 20:47:06 +0000

    make configs key unique

---
+++ server/db/migrations/20160517204305-config-key-unique-migration.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    return queryInterface.changeColumn('Configs', 'key', {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true
+    });
+  },
+
+  down: function (queryInterface, Sequelize) {
+    return queryInterface.changeColumn('Configs', 'key', {
+      type: Sequelize.STRING
+    });
+  }
+};
