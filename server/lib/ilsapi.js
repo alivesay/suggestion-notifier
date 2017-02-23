c126e1a8bdcae2833486da3ef11ba3fc1a64b295
Andrew Livesay <andrew.livesay@gmail.com>
2017-02-23 01:31:07 +0000

    clean up

--- server/lib/ilsapi.js
+++ server/lib/ilsapi.js
@@ -1,8 +1,15 @@
 'use strict';
 
+
+/*
+ * This is a temporary mini ILS API layer and will be replaced by the full
+ * modular API layer in the future.
+ */
+
 var _ = require('lodash');
 var request = require('request');
 var util = require('util');
+var cheerio = require('cheerio');
 
 var settings = require('../config/settings');
 // https://catalog.library.edu:54620/PATRONAPI/21913000482538
@@ -53,7 +60,8 @@ var ILSApi = {
 
     var self = this;
     var results = {};
-console.log(record)
+    // TODO: hack
+    record = record.length < 14 ? "211680" + record : record;
     var requestOptions = {
       uri:  util.format('https://%s:%d/PATRONAPI/%s/dump',
         settings.ilsOptions.catalog.hostname,
@@ -83,12 +91,56 @@ console.log(record)
       });
 
       if (_.size(results) > 0) {
+        results['emailAddress'] = undefined;
         return callback(null, results);
       }
       
-      return callback('patron not found: ' + record, null);
+      return callback('Patron not found: ' + record, null);
     });
 
+  },
+
+  getBib: function getBib (record, callback) {
+    var self = this;
+    var results = {};
+
+    // scrape record from webpac
+    var recordURI = util.format('https://%s/search~S1/,?%s',
+        settings.ilsOptions.catalog.hostname,
+        record
+    );
+
+    var requestOptions = {
+      uri:  recordURI,
+      // TODO: hack
+      strictSSL: false,
+      rejectAuthorization: false
+    };
+ 
+    request(requestOptions, function (error, response, body) {
+      if (error) {
+        return callback(error, null);
+      }
+
+      var $ = cheerio.load(body);
+
+      results.recordURI = recordURI;
+      results.bibNumber = record;
+
+      // scrape isbn
+      // TODO: refactor this...
+      results.isbn = $('td.bibInfoLabel:contains("ISBN")').next('td').text().split(' ')[0];
+      console.log('ISBN: ', results.isbn);
+
+      // scrape title
+      results.title = $("td.bibInfoData > strong").text();
+      if (results.title) {
+        return callback(null, results);
+      }
+      
+      return callback('Bib record not found: ' + record, null);
+    });
+   
   }
 
 };
